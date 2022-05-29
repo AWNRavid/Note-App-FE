bb1507b677cff0016d4004ae42c475b08fe21fdb
unknown <afwan.rizky96@gmail.com>
2022-05-29 19:00:08 +0700

    adding delete button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComp from './Components/NavbarComp';
 import NoteContainer from './Components/NoteContainer';
-
+import { useState } from 'react';
+import { Button, Container } from 'react-bootstrap';
 
 function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      title: 'what',
+      content: 'the',
+    },
+    {
+      id: 2,
+      title: 'learn react for dummies',
+      content: "i don't understand react",
+    },
+    {
+      id: 3,
+      title: 'please',
+      content: 'help',
+    },
+    {
+      id: 4,
+      title: 'Lorem, ipsum.',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam laboriosam rerum dignissimos officiis et earum nesciunt excepturi sunt odio tempore. Laborum, alias harum libero totam nobis ea omnis illo ab quae aperiam culpa amet beatae. Quasi neque voluptate natus magni aliquam, sequi incidunt explicabo quisquam ducimus quo, ex, quam blanditiis?',
+    },
+    {
+      id: 5,
+      title: '',
+      content: '',
+    },
+  ]);
+
+  const handleAddNewNote = () => {
+    const id = notes[notes.length - 1].id + 1;
+
+  }
+
+  // console.log(notes[notes.length - 1]);
+
+  const handleDelete = (id) => {
+    const noteList = notes.filter((note) => {
+      return note.id !== id
+    })
+    setNotes(noteList);
+  }
+
   return (
     <div>
-       <NavbarComp />
-       <NoteContainer />
+      <NavbarComp />
+      <Container className='mt-5'>
+        <Button onClick={handleAddNewNote}>Add New Note</Button>
+      </Container>
+      <NoteContainer notes={notes} setNotes={setNotes} handleDelete={handleDelete} />
+      {/* <textarea name="" id="" cols="30" rows="10">test</textarea> */}
     </div>
   );
 }
--- src/Components/Note.js
+++ src/Components/Note.js
@@ -1,14 +1,47 @@
-import React from 'react'
-import "./Note.css"
+import React, { useState } from 'react';
+import './Note.css';
+import { Button } from 'react-bootstrap';
 
-function Note(props) {
-   
+function Note({ note, setNotes, handleDelete }) {
+  const [noteContent, setNoteContent] = useState(note.content);
+
   return (
-    <div className='note' style={{backgroundColor:props.note.color}}>
+    <div className="note">
+      <h3>{note.title}</h3>
+      {/* <hr /> */}
+      {/* <p className="note-text">{note.content}</p> */}
+      <textarea
+        className="note-content"
+        name=""
+        id=""
+        cols="30"
+        rows="10"
+        value={noteContent}
+        onChange={(e) => {
+          return setNoteContent(e.target.value);
+        }}
+        
+      >
+        {note.content}
+      </textarea>
+      {/* <input className='input-text' type="text" /> */}
+      <Button
+        className="mt-2"
+        onClick={() => {
+          handleDelete(note.id);
+        }}
+      >
+        delete
+      </Button>
+    </div>
+  );
+}
+
+export default Note;
+
+/* 
+<div className='note' style={{backgroundColor:props.note.color}}>
         <textarea className='note-text' defaultValue={props.note.text}/>
         <p>{props.note.time}</p>
     </div>
-  )
-}
-
-export default Note
+*/
