789ed0026321adc883406ce09a4cc07ebddad3ba
unknown <afwan.rizky96@gmail.com>
2022-06-18 05:19:45 +0700

    change baseURL

--- src/api/notes.js
+++ src/api/notes.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:3100/',
-  // baseURL: 'https://vast-sierra-64095.herokuapp.com/',
+  // baseURL: 'http://localhost:3100/',
+  baseURL: 'https://vast-sierra-64095.herokuapp.com/',
   headers: {
     'Authorization': 'Bearer <token>',
     'X-Custom-Header': 'foobar',
